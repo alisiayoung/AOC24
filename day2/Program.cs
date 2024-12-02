580c64feba62b0c93b9e9ae0ffe6527cc7edbe98
Alisia Nicole Young <ayoung.mmt-b2022@fh-salzburg.ac.at>
2024-12-02 22:28:07 +0100

    day2 done

---
+++ day2/Program.cs
@@ -0,0 +1,84 @@
+﻿// the most spaghetti spaghetti code can get
+using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "input.txt";
+
+        int safeCount = 0;
+
+        foreach(string line in File.ReadLines(filePath))
+        {
+            int[] levels = line.Split(new string[] { " " }, StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
+
+            if (IsReportSafe(levels)) 
+            {
+                safeCount++;
+                continue;
+            }
+
+            bool dampenerMakesSafe = false;
+
+            for (int i = 0; i < levels.Length; i++)
+            {
+                int[] modifiedLevels = levels.Where((_, index) => index != i).ToArray();
+
+                if (IsReportSafe(modifiedLevels))
+                {
+                    dampenerMakesSafe = true;
+                    break;
+                }
+            }
+
+            if (dampenerMakesSafe)
+                safeCount++;
+        }
+
+        Console.WriteLine(safeCount);  
+    }
+
+    static bool IsReportSafe(int[] levels)
+    {
+        bool isSafe = true;
+
+        for (int i = 0; i < levels.Length - 1; i++)
+        {
+            if (levels[i] == levels[i + 1]) 
+            {
+                isSafe = false;
+                break;
+            }
+            else if (i > 0 && levels[i] < levels[i + 1] && levels[i] < levels[i - 1]) 
+            {
+                isSafe = false;
+                break;
+            }
+            else if (i > 0 && levels[i] > levels[i + 1] && levels[i] > levels[i - 1])
+            {
+                isSafe = false;
+                break;
+            }
+            else if (levels[i] > levels[i + 1] && levels[i] - levels[i + 1] > 3)
+            {
+                isSafe = false;
+                break;
+            }
+            else if (levels[i] < levels[i + 1] && levels[i + 1] - levels[i] > 3)
+            {
+                isSafe = false;
+                break;
+            }
+        }
+
+        if (isSafe)
+            return true;
+        else
+            return false;
+    }
+}
