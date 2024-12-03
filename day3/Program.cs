4b9684e0667d9acd47d534345af42ef657d78aa3
Alisia Young <alisia.young@spar-ics.com>
2024-12-03 14:48:15 +0100

    day3 part 1

---
+++ day3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string inputPath = "input.txt";
+        string input = File.ReadAllText(inputPath);
+
+        Regex regex = new Regex(@"mul\((?<X>\d+),(?<Y>\d+)\)");
+        MatchCollection matches = regex.Matches(input);
+
+        int sum = 0;
+
+        foreach (Match match in matches)
+        {
+            if (match.Success)
+            {
+                int x = int.Parse(match.Groups["X"].Value);
+                int y = int.Parse(match.Groups["Y"].Value);
+
+                sum += x * y;
+            }
+        }
+
+        Console.WriteLine($"Total sum: {sum}");
+    }
+}
